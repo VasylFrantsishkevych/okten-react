354336511126bd10b7ee324153a07dc1f7d92657
VasylFrantsishkevych <vasul.fr@gmail.com>
2022-01-12 12:33:34 +0200

    classwork 1

--- src/components/mission/Missions.js
+++ src/components/mission/Missions.js
@@ -1,21 +1,27 @@
 import {useEffect, useState} from "react";
 
 import Mission from "./Mission";
-
 import {getMissions} from "../services/api";
-
 
 const Missions = () => {
     const [missions, setMissions] = useState([]);
 
     useEffect( () => {
         getMissions().then(mission =>
-            setMissions(mission.filter(missionYear => missionYear.launch_year !== '2020')));
+            setMissions(mission));
     },[]);
+
+    const missionFilter = missions.filter(missionYear => missionYear.launch_year !== '2020');
+
     return (
         <>
-            {missions.map(value => <Mission key={value.flight_number} mission_name={value.mission_name}
-            launch_year={value.launch_year} img={value.links.mission_patch_small}/>)}
+            {
+                missionFilter.map(value => <Mission
+                    key={value.flight_number}
+                    mission_name={value.mission_name}
+                    launch_year={value.launch_year}
+                    img={value.links.mission_patch_small}/>)
+            }
         </>
     );
 };
