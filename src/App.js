b9a0e6a099b846aafa3ab29af094a5684544ee13
VasylFrantsishkevych <vasul.fr@gmail.com>
2022-01-29 18:19:26 +0200

    useForm

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {useState} from "react";
+import {Cars, Form} from "./components";
 
 function App() {
-  return (
-    <div className="App">
 
+  const [cars, setCars] = useState([]);
+  //функція яка приймає дані з форми
+  const getFormData = (data) => {
+      setCars([...cars, {id:new Date().getTime(), ...data}])
+  }
+  //функція видалення з бази даних
+  const getCarId = (id) => {
+      setCars(cars.filter(car => car.id !== id))
+  }
+
+  return (
+    <div>
+      <Form getFormData={getFormData}/>
+      <Cars cars={cars} getCarId={getCarId}/>
     </div>
   );
 }
