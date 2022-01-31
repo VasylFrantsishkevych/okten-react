a021a24239afaa4684f8dec2f1eb0efac27b0132
VasylFrantsishkevych <vasul.fr@gmail.com>
2022-01-31 13:16:39 +0200

    redux async

--- src/components/Cars/Cars.js
+++ src/components/Cars/Cars.js
@@ -1,10 +1,15 @@
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
+import {useEffect} from "react";
 
 import {Car} from "../Car/Car";
+import {getAllCars} from "../../store/car.slice";
 
 const Cars = () => {
-
-    const {cars} = useSelector(state => state['carReducer']);
+    const {cars, status, error} = useSelector(state => state['carReducer']);
+    const dispatch = useDispatch();
+    useEffect(() => {
+        dispatch(getAllCars())
+    },[])
 
     return (
         <div>
--- src/store/car.slice.js
+++ src/store/car.slice.js
@@ -1,21 +1,53 @@
-import {createSlice} from "@reduxjs/toolkit";
+import {createSlice, createAsyncThunk} from "@reduxjs/toolkit";
+
+import {carService} from "../services";
+
+
+export const getAllCars = createAsyncThunk(
+    'carSlice/getAllCars',
+    async () =>{
+        try {
+            const cars = await carService.getAll();
+            return cars;
+        }catch (error){
+
+        }
+    }
+)
 
 const carSlice = createSlice({
     name: 'carSlice',
     initialState:{
-        cars: []
+        cars: [],
+        status: null,
+        error: null
     },
     reducers:{
         //пушимо в масив id та дані з форми, які ми визначили як data
         addCar:(state, action) => {
             state.cars.push({
+                //створюємо id
                 id: new Date().getTime(),
+                //деструктуризовуємо передані дані
                 ...action.payload.data
             })
         },
         //за допомогою фільтрації будемо видаляти car порівнюючи id
         deleteCar:(state, action) => {
             state.cars = state.cars.filter(car => car.id !== action.payload.id)
+        }
+    },
+    extraReducers:{
+        [getAllCars.pending]:(state, action) => {
+            state.status = 'pending'
+            state.error = null
+        },
+        [getAllCars.fulfilled]:(state, action) => {
+            state.status = 'fulfilled'
+            state.cars = action.payload
+        },
+        [getAllCars.rejected]:(state, action) => {
+
         }
     }
 })
